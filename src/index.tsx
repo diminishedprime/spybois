e66813dcf3fc4e980f7ebbe7b6f6020b54e1e226
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-15 20:06:42 -0700

    Added in firebase auth.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory
+} from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+const firebaseConfig = {
+  apiKey: "AIzaSyAsz9rfRC01eFIfo_FvZ2x3-2DHf_2Ulws",
+  authDomain: "spy-bois.firebaseapp.com",
+  databaseURL: "https://spy-bois.firebaseio.com",
+  projectId: "spy-bois",
+  storageBucket: "spy-bois.appspot.com",
+  messagingSenderId: "154079183942",
+  appId: "1:154079183942:web:36910aa3b42e5406e0b647",
+  measurementId: "G-V2NY7J0W6T"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
 const Lobby = () => {
   return <div>Lobby</div>;
@@ -12,17 +33,55 @@ const Game = () => {
   return <div>Game</div>;
 };
 
+const SignIn = () => {
+  const signIn = React.useCallback(() => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth
+      .signInWithPopup(provider)
+      .then(() => {
+        // redirect to home page after login
+      })
+      .catch(e => {
+        // TODO - handle login error.
+      });
+  }, []);
+  return <Button onClick={signIn}>Login With Google</Button>;
+};
+
+const SignOut = () => {
+  const signOut = React.useCallback(() => {
+    auth.signOut();
+  }, []);
+  return <Button onClick={signOut}>Sign Out</Button>;
+};
+
 const App = () => {
-  // TODO add firebase login.
-  console.log("hi???");
+  const history = useHistory();
+  const [user, setUser] = React.useState<firebase.User>();
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (!user) {
+        history.push("/login");
+      } else {
+        setUser(user);
+        history.push("/");
+      }
+    });
+  }, []);
+
   return (
     <div>
+      {user !== undefined && <SignOut />}
       <Switch>
         <Route exact path={["/lobby", "/"]}>
           <Lobby />
         </Route>
         <Route path="/game">
           <Game />
+        </Route>
+        <Route path="/login">
+          <SignIn />
         </Route>
       </Switch>
     </div>
