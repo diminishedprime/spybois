5bbfee4142e4a99f0fcd973a66fd8e84495caffd
Matt Hamrick <matthewjhamrick@gmail.com>
2020-04-18 12:16:55 -0700

    Updated the loader view to work with the compound hint type.

--- src/game/LeaderView.tsx
+++ src/game/LeaderView.tsx
@@ -17,37 +17,31 @@ interface Props {
 }
 const LeaderView: React.FC<Props> = ({ gameData, player }) => {
   const leader = isLeader(gameData, player);
-  const [localHint, setLocalHint] = React.useState<
-    Partial<HintData & { remainingGuesses: HintNumber }> | undefined
-  >(gameData.currentHint);
+  const [localHint, setLocalHint] = React.useState<HintData>(() => {
+    if (gameData.currentHint !== undefined) {
+      return gameData.currentHint;
+    } else {
+      return {
+        hint: "",
+        hintNumber: 1,
+        remainingGuesses: 1,
+        submitted: false,
+        team: gameData.currentTeam,
+      };
+    }
+  });
   const submit = React.useCallback(() => {
-    if (localHint === undefined) {
-      return;
-    }
-    if (localHint.hintNumber === undefined) {
-      return;
-    }
-    if (localHint.remainingGuesses === undefined) {
-      return;
-    }
-    if (localHint.hint === undefined) {
-      return;
-    }
     if (localHint.hint === "") {
       return;
     }
-    if (localHint.team === undefined) {
-      return;
-    }
-    const concreteHint: HintData = {
-      hint: localHint.hint,
-      hintNumber: localHint.hintNumber,
-      remainingGuesses: localHint.remainingGuesses,
-      team: localHint.team,
-      submitted: true,
-    };
-    submitHint(db, gameData, concreteHint);
+    submitHint(db, gameData, localHint);
   }, [localHint, gameData]);
+
+  React.useEffect(() => {
+    if (localHint.remainingGuesses !== localHint.hintNumber) {
+      setLocalHint({ ...localHint, remainingGuesses: localHint.hintNumber });
+    }
+  }, [localHint]);
 
   React.useEffect(() => {
     // Keep the local hint's team in sync with the game's current team.
@@ -58,25 +52,73 @@ const LeaderView: React.FC<Props> = ({ gameData, player }) => {
     ) {
       setLocalHint((current) => ({ ...current, team: gameData.currentTeam }));
     }
-  }, [localHint, gameData.currentTeam]);
+  }, [localHint.team, gameData.currentTeam]);
+
+  const bumpNum = React.useCallback(
+    (direction: "up" | "down") => () => {
+      setLocalHint((currentHint) => {
+        const currentNumber = currentHint.hintNumber;
+        if (currentNumber === "zero" && direction === "down") {
+          return currentHint;
+        }
+        if (currentNumber === "infinity" && direction === "up") {
+          return currentHint;
+        }
+
+        let nuNumber: HintNumber;
+        if (currentNumber === "infinity" && direction === "down") {
+          // TODO - Playtest to figure out if this number is big enough???
+          nuNumber = 9;
+        } else if (currentNumber === "zero" && direction === "up") {
+          nuNumber = 1;
+        } else {
+          const bump = direction === "down" ? -1 : 1;
+          if (typeof currentNumber !== "number") {
+            // This path shouldn't be reachable.
+            return currentHint;
+          }
+          nuNumber = currentNumber + bump;
+        }
+        if (nuNumber === 0) {
+          nuNumber = "zero";
+        }
+        if (nuNumber === 10) {
+          nuNumber = "infinity";
+        }
+        return { ...currentHint, hintNumber: nuNumber };
+      });
+    },
+    []
+  );
 
   if (!leader) {
     return null;
   }
 
+  console.log({ localHint });
+
   return (
     <>
       <TextField
-        value={localHint}
-        disabled={gameData.currentHint?.submitted}
+        value={localHint?.hint || ""}
+        disabled={localHint.submitted}
         onChange={(e) => setLocalHint({ ...localHint, hint: e.target.value })}
       />
       <Button
-        disabled={
-          gameData.currentHint?.submitted ||
-          localHint === "" ||
-          gameData.currentHint?.remainingGuesses === undefined
-        }
+        disabled={localHint.hintNumber === "zero" || localHint.submitted}
+        onClick={bumpNum("down")}
+      >
+        -
+      </Button>
+      {localHint.hintNumber}
+      <Button
+        disabled={localHint.hintNumber === "infinity" || localHint.submitted}
+        onClick={bumpNum("up")}
+      >
+        +
+      </Button>
+      <Button
+        disabled={localHint.submitted || localHint.hint === ""}
         onClick={submit}
       >
         Submit Hint
